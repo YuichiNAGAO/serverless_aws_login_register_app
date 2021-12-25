02d7e09d8247a1081a3ca7682f5488e087d6f259
YuichiNAGAO <azkgfa03nagao@gmail.com>
2021-12-26 00:32:26 +0900

    comit everything

--- backend/upload.sh
+++ backend/upload.sh
@@ -1,2 +1,2 @@
 zip -rq archive.zip features/ utils/ node_modules/ index.js package.json
-aws lambda update-function-code   --function-name sample-app-backend  --zip-file fileb://archive.zip 
+aws lambda update-function-code --function-name sample-app-backend  --zip-file fileb://archive.zip 
